fd2b20c5e6b6a0928de73f828aa24c8d74573a3b
anishsharma21 <anisharma2103@gmail.com>
2024-06-06 22:23:41 +1000

    - Implemented add function to Binary class that can add any 2 unsigned binary numbers

--- miscellaneous/convert-binary-to-decimal/binary-class.py
+++ miscellaneous/convert-binary-to-decimal/binary-class.py
@@ -16,20 +16,30 @@ class Binary:
         else:
             raise InvalidBinaryOperationException("Input is not of type Binary")
     
-    def _adder(self, b1, b2): # not done
+    def _adder(self, b1, b2):
         final_b = ''
         carry = '0'
         for i in range(len(b1), 0, -1):
-            c = '0' if b1[i-1] == b2[i-1] else '1'
-            if b1[i-1] == '1' and c == '0':
-                carry = '1'
-            final_b = str(c) + str(final_b)
-            if i-1 == 0 and c == '0' and b1[i-1] == '1':
-                final_b = str(c) + str(final_b)
+            if carry == '0':
+                if b1[i-1] == b2[i-1]:
+                    carry = '1' if b1[i-1] == '1' else '0'
+                    final_b = '0' + final_b
+                else:
+                    carry = '0'
+                    final_b = '1' + final_b
+            else:
+                if b1[i-1] == b2[i-1]:
+                    carry = '1' if b1[i-1] == '1' else '0'
+                    final_b = '1' + final_b if b1[i-1] == '0' else '0' + final_b
+                else:
+                    carry = '1'
+                    final_b = '0' + final_b
+            if i - 1 == 0 and carry == '1':
+                final_b = '1' + final_b
         return final_b
 
-b1 = Binary('011')
-b2 = Binary('101')
+b1 = Binary('10100')
+b2 = Binary('01101')
 try:    
     print(b1.add(b2))
 except InvalidBinaryOperationException as e:
